be8c7d2c3447a1db8ca2d94c077cf814355e183c
Kb10r <54547103+Kb10r@users.noreply.github.com>
2021-01-31 23:51:33 -0700

    Initial Upload for Activity2c

---
+++ Activity2c/Controllers/AppointmentController.cs
@@ -0,0 +1,29 @@
+﻿using Activity2c.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Activity2c.Controllers
+{
+    public class AppointmentController : Controller
+    {
+        static List<AppointmentModel> appointments = new List<AppointmentModel>();
+        public IActionResult Index()
+        {
+            return View(appointments);
+        }
+
+        public IActionResult Create() 
+        {
+            return View();
+        }
+
+        public IActionResult Details(AppointmentModel am) 
+        {
+            appointments.Add(am);
+            return View("Details", am);
+        }
+    }
+}
---
+++ Activity2c/Models/AppointmentModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Activity2c.Models
+{
+    public class AppointmentModel
+    {
+        [Required][StringLength(20, MinimumLength =4)]
+        [DisplayName("Patients Full Name")]
+        public string patientName { get; set; }
+
+        [Required][DataType(DataType.DateTime)]
+        [DisplayName("Date of the Appointment")]
+        public DateTime appointmentDate { get; set; }
+
+        [Required][DataType(DataType.Currency)]
+        [DisplayName("Net Worth of patient")]
+        public decimal patientNetWorth { get; set; }
+
+        [Required]
+        [DisplayName("Last Name of Doctor")]
+        public string doctorName { get; set; }
+
+        [Required][Range(minimum:5, maximum:10)]
+        [DisplayName("How much pain are you currently in? 1(low) - 10(high)")]
+        public int painLevel { get; set; }
+
+
+
+    }
+}
